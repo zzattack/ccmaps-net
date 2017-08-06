d2c8680f2c8851a2e16a40eb690e3ca9d19826b2
Frank Razenberg <frank@zzattack.org>
2017-08-06 16:50:03 +0200

    ass option to disable tile fixup

--- CNCMaps.Renderer.GUI/Properties/Settings.Designer.cs
+++ CNCMaps.Renderer.GUI/Properties/Settings.Designer.cs
@@ -9,11 +9,11 @@
 //------------------------------------------------------------------------------
 
 namespace CNCMaps.GUI.Properties {
-
-    [System.Configuration.SettingsProvider(typeof(CustomSettingsProvider))]
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+    public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
@@ -416,6 +416,18 @@ namespace CNCMaps.GUI.Properties {
             }
             set {
                 this["markers"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool fixuptiles {
+            get {
+                return ((bool)(this["fixuptiles"]));
+            }
+            set {
+                this["fixuptiles"] = value;
             }
         }
     }
