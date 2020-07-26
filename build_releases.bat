4ca8df12bef68e45244b3998552dd6b879e2f091
Starkku <starkku.ppmod@gmail.com>
2020-07-26 17:09:15 +0300

    Improvements to build_releases.bat.
    - Removed obsolete commands.
    - Added support for non-Enterprise editions of Visual Studio 2019.
    - Delete existing binaries before attempting to build to ensure that installer does not get created with outdated binaries in case build fails.

--- build_releases.bat
+++ build_releases.bat
@@ -1,31 +1,27 @@
 set VER=2.4.0
 set MAKENSIS="%PROGRAMFILES(X86)%\nsis\makensis.exe"
 
-del CNCMaps_*.zip
-del CNCMaps_setup_*.exe
-del CNCMaps/bin/*.*
-del CNCMaps/obj/*.*
-del CNCMaps GUI/obj/*.*
-del CNCMaps GUI/obj/*.*
+del CNCMaps_release_*.exe
+del CNCMaps_debug_*.exe
+rmdir /s /q CNCMaps.Engine\bin\
+rmdir /s /q CNCMaps.FileFormats\bin\
+rmdir /s /q CNCMaps.Renderer\bin\
+rmdir /s /q CNCMaps.Renderer.GUI\bin\
+rmdir /s /q CNCMaps.Shared\bin\
 
-set VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\
-set DevEnvDir=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
+set "VSINSTALLDIR=%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Enterprise\"
+if not exist "%VSINSTALLDIR%" (
+set "VSINSTALLDIR=%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Professional\"
+)
+if not exist "%VSINSTALLDIR%" (
+set "VSINSTALLDIR=%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Community\"
+)
+set DevEnvDir=%VSINSTALLDIR%Common7\IDE\
+call "%VSINSTALLDIR%Common7\Tools\VsDevCmd.bat"
 msbuild kerfcheck.sln 
 
 msbuild CNCMaps.sln /t:restore /t:Build /p:Configuration=Release
 %MAKENSIS% nsisinstaller-rls.nsi
 
-cd CNCMaps/bin/Release
-for /D %%f in (*.exe  *.dll NLog.config) DO (
-	# zip -r -j ../../../CNCMaps_v%VER%_win.zip "%%f"
-)
-
-for /D %%f in (*.exe  *.dll NLog.config OpenTK.dll.config) DO (
-	# zip -r -j ../../../CNCMaps_v%VER%_nix.zip "%%f"
-)
-
-cd ../../../
-
-%MSBUILD% CNCMaps.sln /p:Configuration=Debug
-%MAKENSIS% nsisinstaller-dbg.nsi
+REM msbuild CNCMaps.sln /p:Configuration=Debug
+REM %MAKENSIS% nsisinstaller-dbg.nsi
